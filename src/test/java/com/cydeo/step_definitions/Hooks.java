9eddf6c1526723f2c47c218291ff7a0f295861f1
Ayse <aysenur_aydin@hotmail.fr>
2022-06-15 17:37:18 +0200

    day 15 Hooks.java

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,7 +5,9 @@ In the class we will be able to pass pre & post conditions to each scenario and 
  */
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import org.junit.runners.Parameterized;
 
 public class Hooks {
@@ -22,5 +24,14 @@ public class Hooks {
         System.out.println("====Scenario ended/Take screenshot if failed");
     }
 
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("----------> applying setup using @BeforeStep");
+    }
+
+    @AfterStep
+    public void afterStep(){
+        System.out.println("---------> applying tearDown using @AfterStep");
+    }
 
 }
